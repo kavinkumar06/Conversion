085a97214979b7cc2af196858bd04ee14062541a
kavinkumar06 <kavinm09@gmail.com>
2017-07-17 18:08:35 +0530

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,15 @@
+# Conversion
+import java.io.*;
+import java.util.*;
+public class Conversion
+{
+  public static void main(String args[])throws IOException
+  {
+  int i;
+  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+   String s="200";
+   i=Integer.parseInt(br.readLine());
+   System.out.println(s+100);
+   System.out.println(i+100);
+   }
+   }
